4321bf669d5a58bb0adabcb1dc1681741a2cded5
Алексей Савельев <mtptz21@gmail.com>
2022-12-23 00:27:09 +0300

    feat: add new file

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          'style-loader',
+          // MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    // new MiniCssExtractPlugin({filename:'styles.css'}),
+  ],
+
+  stats: 'errors-only',
+  devServer: {
+    port: 4444,
+    open: true,
+  },
+};
